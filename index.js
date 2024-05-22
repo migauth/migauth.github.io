0d5b00eba6033639f0e537cc171ff5652be8ba5a
Michael Gauthier <mikegauthier222@hotmail.com>
2024-05-22 16:35:40 -0700

    add trailing squares animation and updated resume

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ function togglePage(currentPage) {
   const homePage = document.getElementById('home-page');
   const pages = document.querySelectorAll('.page');
 
-  
-  
   pages.forEach(page => {
     
     if (page.id === currentPage) {
@@ -22,3 +20,56 @@ function togglePage(currentPage) {
     }
   });
 }
+
+//Canvas
+
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const trailLength = 90;
+const trail = [];
+
+class Square {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 10 + 20;
+    this.color = `hsl(${Math.random() * 360}, 100%, 80%)`;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+  }
+}
+
+function drawTrail() {
+  for (let i = 0; i < trail.length; i++) {
+    const transparency = (i + 1) / trailLength;
+    ctx.globalAlpha = transparency;
+    trail[i].draw();
+  }
+}
+
+function updateTrail(x, y) {
+  if (trail.length >= trailLength) {
+    trail.shift();
+  }
+  trail.push(new Square(x, y));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawTrail();
+  requestAnimationFrame(animate);
+}
+
+window.addEventListener('mousemove', function(e) {
+  updateTrail(e.x, e.y);
+});
+
+animate();
